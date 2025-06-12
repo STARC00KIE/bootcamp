9525451fd969cf0779c8e7994168132385a76482
STARC00KIE <os4155@gmail.com>
2025-06-13 08:39:00 +0900

    다익스트라 알고리즘 구현에서 거리 배열 변수명을 'dist'에서 'distance'로 변경하고, 감염된 컴퓨터 개수를 계산하는 방식을 개선하여 가독성을 높임.

--- codingtest_study/dijkstra/b_10282.py
+++ codingtest_study/dijkstra/b_10282.py
@@ -9,20 +9,23 @@ for _ in range(n): # 테스트 케이스의 개수
         a, b, s = map(int, sys.stdin.readline().split())
         graph[b].append((a, s))
     
-    dist = [float('inf')] * (n + 1)
-    dist[c] = 0
+    distance = [float('inf')] * (n + 1)
+    distance[c] = 0
     H = [(0, c)]
 
     while H:
         time, node = heapq.heappop(H)
-        if dist[node] < time:
+        if distance[node] < time:
             continue
         for next_node, cost in graph[node]:
             new_time = time + cost
-            if dist[next_node] > new_time:
-                dist[next_node] = new_time
+            if distance[next_node] > new_time:
+                distance[next_node] = new_time
                 heapq.heappush(H, (new_time, next_node))
-    result = dist # 
+    result = distance # 
 
-    infected = [time for time in result if time != float('inf')] # 감염된 컴퓨터의 개수
+    infected = [] # 감염된 컴퓨터의 개수
+    for time in result:
+        if time != float('inf'):
+            infected.append(time)
     print(len(infected), max(infected)) # 출력은 감염된 컴퓨터의 개수, 걸리는 시간
